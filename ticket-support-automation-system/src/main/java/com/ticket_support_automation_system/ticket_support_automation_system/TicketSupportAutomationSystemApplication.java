b592ab13dd484d5c2175c70c3d52195fd0656f9d
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-13 10:07:33 +0600

    initialized starting

---
+++ ticket-support-automation-system/src/main/java/com/ticket_support_automation_system/ticket_support_automation_system/TicketSupportAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticket_support_automation_system.ticket_support_automation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSupportAutomationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSupportAutomationSystemApplication.class, args);
+	}
+
+}
---
+++ ticket-support-automation-system/src/test/java/com/ticket_support_automation_system/ticket_support_automation_system/TicketSupportAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket_support_automation_system.ticket_support_automation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSupportAutomationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
