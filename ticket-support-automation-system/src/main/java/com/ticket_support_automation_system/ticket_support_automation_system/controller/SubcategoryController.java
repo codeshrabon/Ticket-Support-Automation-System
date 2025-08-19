5e1032a17df4aaa8da8995a75aef77ae4f3b49df
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-19 18:32:42 +0600

    created subcategory that related to category

--- ticket-support-automation-system/src/main/java/com/ticket_support_automation_system/ticket_support_automation_system/controller/SubcategoryController.java
+++ ticket-support-automation-system/src/main/java/com/ticket_support_automation_system/ticket_support_automation_system/controller/SubcategoryController.java
@@ -1,9 +1,39 @@
 package com.ticket_support_automation_system.ticket_support_automation_system.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ticket_support_automation_system.ticket_support_automation_system.model.Groups.Subcategory;
+import com.ticket_support_automation_system.ticket_support_automation_system.service.CategoryService;
+import com.ticket_support_automation_system.ticket_support_automation_system.service.SubcategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subcategory")
 public class SubcategoryController {
+
+    @Autowired
+    private final SubcategoryService subcategoryService;
+
+    public SubcategoryController(SubcategoryService subcategoryService) {
+        this.subcategoryService = subcategoryService;
+    }
+
+
+    // get subcategory by categoryId
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Subcategory>> getSubcategoryByCategory(@PathVariable Long categoryId) {
+        List<Subcategory> getSubcategory = subcategoryService.GetSubcategoryByCategory(categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(getSubcategory);
+    }
+
+    // add subcategory by category ID
+    @PostMapping("/add/{categoryId}")
+    public ResponseEntity<Subcategory> addSubcategory(@PathVariable Long categoryId, @RequestBody Subcategory subcategory) {
+        Subcategory saveSubcategory = subcategoryService.AddSubcategoryToCategory(categoryId,subcategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveSubcategory);
+    }
+
 }
--- ticket-support-automation-system/src/main/java/com/ticket_support_automation_system/ticket_support_automation_system/service/SubcategoryService.java
+++ ticket-support-automation-system/src/main/java/com/ticket_support_automation_system/ticket_support_automation_system/service/SubcategoryService.java
@@ -1,16 +1,43 @@
 package com.ticket_support_automation_system.ticket_support_automation_system.service;
 
+import com.ticket_support_automation_system.ticket_support_automation_system.model.Groups.Category;
+import com.ticket_support_automation_system.ticket_support_automation_system.model.Groups.Subcategory;
+import com.ticket_support_automation_system.ticket_support_automation_system.repository.CategoryRepository;
 import com.ticket_support_automation_system.ticket_support_automation_system.repository.SubcategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubcategoryService {
 
-    @Autowired
+
     private final SubcategoryRepository subcategoryRepository;
 
-    public SubcategoryService(SubcategoryRepository subcategoryRepository) {
+
+    private final CategoryRepository categoryRepository;
+
+    public SubcategoryService(SubcategoryRepository subcategoryRepository, CategoryRepository categoryRepository) {
         this.subcategoryRepository = subcategoryRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    //get subcategory by categoryId
+    public List<Subcategory> GetSubcategoryByCategory(Long categoryId) {
+        return subcategoryRepository.findAll()
+                .stream()
+                .filter(sub -> sub.getCategory().getId().equals(categoryId))
+                .toList();
+    }
+
+    public Subcategory AddSubcategoryToCategory(Long categoryId, Subcategory subcategory) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+        subcategory.setCategory(category);
+
+        return subcategoryRepository.save(subcategory);
+
+
     }
 }
